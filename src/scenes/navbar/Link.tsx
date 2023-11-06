311a86ceb046f60d720a36732be42738e203da86
ArtengoClan <135038710+ArtengoClan@users.noreply.github.com>
2023-11-06 21:49:51 +0100

    first commit

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,26 @@
+import { SelectedPage } from '@/shared/types';
+import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+type Props = {
+    page: string;
+    selectedPage: SelectedPage;
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const Link = ({page, selectedPage, setSelectedPage}: Props) => {
+    const lowerCasePge = page.toLowerCase().replace(/ /g, '') as SelectedPage
+  return (
+    <AnchorLink
+        className={`${selectedPage === lowerCasePge ? 'text-primary-500' : ''}
+            transition duration-500 hover:text-primary-300
+        `}
+        href={`#${lowerCasePge}`}
+        onClick={() => setSelectedPage(lowerCasePge)}
+    >
+        {page}
+    </AnchorLink>
+  )
+}
+
+export default Link
